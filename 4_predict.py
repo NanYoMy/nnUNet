017f72b866f38b3914aaf55bee9a011458fedbe0
dwb <moon_well@live.cn>
2025-07-25 16:00:04 +0800

    add

--- 4_predict.py
+++ 4_predict.py
@@ -2,6 +2,8 @@ from nnunetv2.inference.predict_from_raw_data import predict_entry_point as nnUN
 import os
 import shutil   
 
+#D:/Users/dwb/anaconda3/envs/torch2.7.1/python.exe g:/nnUnetV2/code/4_predict.py -i G:\nnUnetV2\data_result\nnUNet_raw\Dataset100_CPSegmentation\imagesTr -o G:\nnUnetV2\data_result\nnUNet_raw\Dataset100_CPSegmentation\imagesTr_pred -d 100 -c 2d -f 1
+
 if __name__ == "__main__":
 
     os.environ['OMP_NUM_THREADS'] = r'1'
