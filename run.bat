87fc46e995cb969f43f0b0d672554824c3b7b00a
dwb <moon_well@live.cn>
2025-07-27 16:32:15 +0800

    add

--- run.bat
+++ run.bat
@@ -2,12 +2,9 @@
 
 @REM 0.train_prepare_CP_convert_to_png.py
 
-
 @REM 1_prepare_CP_for_nnunetv2.py
 
 @REM 2_plan_and_preprocess.py
-
-
 
 @REM 3_train_network.py 100 2d all -tr nnUNetTrainerUNetPP
 
