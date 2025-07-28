494d2f486711f2606640b4a04e06771e306c3f7f
nanyomy <dingwangbin@126.com>
2025-07-28 10:16:26 +0800

    add test

---
+++ run-test.bat
@@ -0,0 +1,38 @@
+@REM 0.test_prepare_CP_convert_to_png.py 
+
+@REM 0.train_prepare_CP_convert_to_png.py
+
+@REM 1_prepare_CP_for_nnunetv2.py
+
+@REM 2_plan_and_preprocess.py
+
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerAttUNet
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerUNetPlain
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerIBUNet_5_4_3
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerIBUNet_5_4
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerIBUNet_5
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerNoDeepSupervision
+
+@REM python 3_train_network.py 100 2d all -tr nnUNetTrainerBN
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\attunet_pred -c 2d -d 100 -f all  -tr nnUNetTrainerAttUNet
+
+
+python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\nodeep_pred -c 2d -d 100 -f all  -tr nnUNetTrainerNoDeepSupervision
+
+python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\unetBN_pred -c 2d -d 100 -f all  -tr nnUNetTrainerBN
+
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\predictions_2 -c 2d -d 100 -f 2
+
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\predictions_3 -c 2d -d 100 -f 3
+
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\predictions_4 -c 2d -d 100 -f 4
+
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\predictions_5 -c 2d -d 100 -f 5
+
+
+@REM python 4_predict.py -i ../data_result/nnUNet_raw/Dataset100_CPSegmentation/imagesTs -o ../data_result\nnUNet_raw\Dataset100_CPSegmentation\predictions_final -c 2d -d 100 -f all
